48e27bf44ccee0f225619b5f4544aa7e188950ac
anhtuan240599 <55918598+anhtuan240599@users.noreply.github.com>
2020-11-08 20:37:44 +0700

    1

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,26 +1,84 @@
 const User = require('../model/User')
+const Deck = require('../model/Deck')
+const Joi = require('@hapi/joi')
+
+const idSchema = Joi.object().keys({
+    userID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
+})
+
+const getUser = async (req,res,next) => {
+    
+    const {userID} = req.value.params
+    const user = await User.findById(userID)
+    console.log(user)
+    return res.status(200).json({user})
+
+}
+
+const getUserDeck = async (req,res,next) => {
+
+    const {userID} = req.params
+    const user = await User.findById(userID).populate('decks')
+    return res.status(200).json({decks : user.decks})
+
+}
 
 const index = async (req,res,next) => {
-   try {
+    
     const users = await User.find({})
     return res.status(200).json({users})
-   } catch (error) {
-       next(error)
-   }
-
 }
 
 const newUser = async (req,res,next) => {
    
     const newUser = new User(req.body)
-  
     await newUser.save()
-
     return res.status(201).json({user : newUser})
   
 }
 
+const newUserDeck = async (req,res,next) => {
+
+    const {userID} = req.params 
+
+    const newDeck = new Deck(req.body)
+
+    const user = await User.findById(userID)
+
+    newDeck.owner = user
+
+    newDeck.save()
+
+    user.decks.push(newDeck._id)
+
+    await user.save()
+
+    res.status(201).json({deck:newDeck})
+}
+
+const replaceUser = async (req,res,next) => {
+
+    const {userID} = req.params
+    const newUser = req.body
+    const result = await User.findByIdAndUpdate(userID, newUser)
+    return res.status(200).json({success : true})
+}
+
+const updateUser = async (req,res,next) => {
+
+    const {userID} = req.params
+    const newUser = req.body
+    const result = await User.findByIdAndUpdate(userID, newUser)
+    return res.status(200).json({success : true})
+
+}
+
 module.exports = {
     index,
-    newUser
+    newUser,
+    getUser,
+    replaceUser,
+    updateUser,
+    getUserDeck,
+    newUserDeck
 }
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,23 @@
 const express = require('express')
 
-const userController = require('../controllers/user.controller')
+const UserController = require('../controllers/user.controller')
 
 const router = require('express-promise-router')()
 
-const UserController = require('../controllers/user.controller')
+const {validateParam , schemas} = require('../helpers/routerHelper')
 
 router.route('/')
     .get(UserController.index)
-    .post(userController.newUser)
+    .post(UserController.newUser)
+
+router.route('/:userID')
+    .get(validateParam(schemas.idSchema,'userID'),UserController.getUser)
+    .put(UserController.replaceUser)
+    .patch(UserController.updateUser)
+
+router.route('/:userID/decks')
+    .get(UserController.getUserDeck)
+    .post(UserController.newUserDeck)
+
 
 module.exports = router
