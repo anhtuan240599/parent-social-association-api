927fa08688f5de6d095bf7cc7bcdf95acf2e8aa2
anhtuan240599 <55918598+anhtuan240599@users.noreply.github.com>
2020-11-15 13:12:58 +0700

    2

---
+++ Authhh/auth/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <header-public />
+    <div class="r">
+      <div class="ct">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderPublic from "./components/PublicHeader";
+export default {
+  components: {
+    HeaderPublic,
+  },
+};
+</script>
+
+<style lang="scss"></style>
