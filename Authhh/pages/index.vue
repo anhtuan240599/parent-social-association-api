98c81f47bcb34e609f4e4703f74122d71e21671a
anhtuan240599 <55918598+anhtuan240599@users.noreply.github.com>
2020-11-19 01:45:52 +0700

    1

---
+++ Authhh/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="(deck, index) in decks" :key="deck._id">
+      <div>
+        <span> {{ deck.description }} </span>
+      </div>
+      <a href="#">
+      <v-for>
+        <img :src="deck.image[0].filename">
+      </v-for>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    try {
+      let response = await $axios.$get("http://localhost:4000/decks");
+      return {
+        decks: response.decks
+      };
+    } catch (err) {}
+  }
+};
+</script>
